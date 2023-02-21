0425027bea008fbf67606862e0cc0227c5099ae1
aestoir <110066720+aestoir@users.noreply.github.com>
2023-02-20 19:43:20 -0500

    database connected and initialized

--- InMyPlate/Models/City.cs
+++ InMyPlate/Models/City.cs
@@ -11,6 +11,7 @@ namespace InMyPlate.Models
         [MinLength(3)]
         public String? Name { get; set; }
         public String? Image { get; set; }
+
         public List<Post>? Posts { get; set; }
         public List<Restaurant>? Restaurants { get; set; }
     }
--- InMyPlate/Models/Restaurant.cs
+++ InMyPlate/Models/Restaurant.cs
@@ -12,13 +12,14 @@ namespace InMyPlate.Models
         [MinLength(3)]
         public String? Name { get; set; }
         public String? Image { get; set; }
-        public List<Post>? Posts { get; set; }
         public int Likes { get; set; }
         public Boolean Save { get; set; }
 
         public int CityId { get; set; }
 
         public City? City { get; set; }
+
+        public List<Post>? Posts { get; set; }
     }
 }
 
